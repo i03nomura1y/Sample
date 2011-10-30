334932443ffb6e0b36ae3ef56034302a7d584025
Yusuke Nomura <i03nomura1y@gmail.com>
2011-10-30 21:17:34 +0900

    update Hello/

---
+++ Hello/hello.cpp
@@ -0,0 +1,12 @@
+// created date : 2011/10/30 12:48:45
+// last updated : 2011/10/30 21:16:57
+// HelloWorld in C++
+// $ g++ hello.cpp -o hello
+// $ ./hello
+
+#include <iostream>
+
+int main(){
+  std::cout << "Hello, World!!" << std::endl;
+  return 0;
+}
