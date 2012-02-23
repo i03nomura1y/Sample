08a02cfeab20043bb4d86e44c01b57df454bac44
Yusuke Nomura <i03nomura1y@gmail.com>
2012-02-24 05:38:23 +0900

    update Class/.gitignore

--- Class/test_shared_ptr.cpp
+++ Class/test_shared_ptr.cpp
@@ -1,5 +1,5 @@
 // created date : 2012/01/09 18:45:41
-// last updated : 2012/01/09 19:15:54
+// last updated : 2012/01/09 22:00:48
 // shared_ptr の実験
 
 #include <iostream>
@@ -8,27 +8,40 @@ using namespace std;
 
 int alloc_cnt = 0;
 
-class Obj{
-public:
-  Obj() { alloc_cnt++; }
-  ~Obj(){ alloc_cnt--; }
-};
-
+class Obj;
 //typedef Obj* Ptr;
 typedef tr1::shared_ptr<Obj> Ptr;
 
-class Container{
+class Obj{
 public:
-  Container(){}
-  void set(Obj* o){ obj = Ptr(o); }
-private:
-  Ptr obj;
+  Obj() { alloc_cnt++; cout << "+" << endl; }
+  ~Obj(){ alloc_cnt--; cout << "-" << endl; }
+  Ptr p;
 };
+
+
 int main(){
   {
-	Container c;
-	c.set(new Obj());
-	c.set(new Obj());
+	Ptr p1;
+	p1 = Ptr(new Obj());
+	cout << "p: " << (bool)p1 << endl;
+	p1 = Ptr(); // デフォルトコンストラクタ: NULL
+	cout << "p: " << (bool)p1 << endl; // NULL なら false
+	p1 = Ptr(new Obj());
+	
+	Obj *a = new Obj();
+	Ptr p2 = Ptr(a);
+	//Ptr p3 = Ptr(a); // error
+
+	Ptr p3 = Ptr(new Obj);
+	p2->p = p3;
+	p3->p = p2;
+	// p2 と p3 がループしてる( p2 <-> p3 )ので、このままだと消えない。
+	p2->p.reset(); // p2 <- p3 の状態にすると消える。
+
+	// 自己参照 も消えない。
+	p1->p = p1;
+	
   }
 
   cout << alloc_cnt << endl;
